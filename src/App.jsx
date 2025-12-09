f355af4999a2bc600bd4993d441c758ccd0063cb
Priyanshushri3011 <priyanshushri3011@gmail.com>
2025-12-09 17:28:50 +0530

    Fourth Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import "./index.css";
 import "./App.css"
 import Allroutes from "./Allroutes";
 import AboutUs from "./Aboutus/Aboutus";
+import WhatsAppFloat from "./Components/WhatsAppFloat";
+
 
 function App() {
   return (
     <>
       <Allroutes/>
       {/* <AboutUs/> */}
+      <WhatsAppFloat/>
+
+      
+      
     </>
   );
 }
